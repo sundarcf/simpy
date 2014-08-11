f3556bc68e5e895e8a82279f4c2fe461fe95e8a8
Sundar M. Vaidya <sundar@cloudfactory.com>
2014-08-11 14:04:17 +0545

    Environments - State access

--- topical_guides/environments.py
+++ topical_guides/environments.py
@@ -1,9 +1,15 @@
 import simpy
 
 def my_proc(env):
-    yield env.timeout(1)
-    return 'Monty Python\'s Flying Circus'
+    while True:
+        print(env.active_process)  # will print "p1"
+        subfunc(env)
+        yield env.timeout(1)
+
+def subfunc(env):
+    print(env.active_process)  # will print "p1"
 
 env = simpy.Environment()
-proc = env.process(my_proc(env))
-env.run(until=proc)
+p1 = env.process(my_proc(env))
+env.active_process  # None
+env.step()
